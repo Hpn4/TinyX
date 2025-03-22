242222ab99d8168acba33bbc94b3f2df6d18cca8
Greg <gregoire.suissa@gmail.com>
2025-03-22 15:08:40 +0100

    (Archi) test of maven

---
+++ tinyX/tinyx-parent/repo-post/src/test/java/com/tinyx/AppTest.java
@@ -0,0 +1,12 @@
+package com.tinyx;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true, "This is a basic test with JUnit 5");
+    }
+}

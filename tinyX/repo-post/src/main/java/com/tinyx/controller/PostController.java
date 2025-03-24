58356ac0865adaa1c150c170f5fe12e2659f7ad2
Bastien Gaulier <bastien.gaulier@epita.fr>
2025-03-24 15:11:37 +0100

    patch(repo-post): formatted with Spotless

--- tinyX/repo-post/src/main/java/com/tinyx/controller/PostController.java
+++ tinyX/repo-post/src/main/java/com/tinyx/controller/PostController.java
@@ -2,75 +2,71 @@ package com.tinyx.controller;
 
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+import java.util.UUID;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
-import java.util.UUID;
-
 /**
- * Handles all the endpoints that are purely post related.
- * Maybe a new endpoint could be later added to update user data (c.f. Miro)
+ * Handles all the endpoints that are purely post related. Maybe a new endpoint could be later added
+ * to update user data (c.f. Miro)
  */
 @Path("/posts")
 public class PostController {
-    @POST
-    @Path("/new")
-    @APIResponses({
-            @APIResponse(responseCode = "200", description = "OK"),
-            @APIResponse(responseCode = "400", description = "Bad user / post format"),
-            @APIResponse(responseCode = "404", description = "User does not exist")
-    })
-    public Response newPostEndpoint(@HeaderParam("X-User") UUID userId) // TODO add post as body
-    {
-        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
-    }
+  @POST
+  @Path("/new")
+  @APIResponses({
+    @APIResponse(responseCode = "200", description = "OK"),
+    @APIResponse(responseCode = "400", description = "Bad user / post format"),
+    @APIResponse(responseCode = "404", description = "User does not exist")
+  })
+  public Response newPostEndpoint(@HeaderParam("X-User") UUID userId) // TODO add post as body
+      {
+    return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+  }
 
-    @DELETE
-    @Path("/delete")
-    @APIResponses({
-            @APIResponse(responseCode = "200", description = "OK"),
-            @APIResponse(responseCode = "400", description = "Bad user"),
-            @APIResponse(responseCode = "401", description = "Cannot delete another user's post"),
-            @APIResponse(responseCode = "404", description = "User / post does not exist")
-    })
-    public Response deletePostEndpoint(@HeaderParam("X-User") UUID userId) // TODO add postId as body
-    {
-        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
-    }
+  @DELETE
+  @Path("/delete")
+  @APIResponses({
+    @APIResponse(responseCode = "200", description = "OK"),
+    @APIResponse(responseCode = "400", description = "Bad user"),
+    @APIResponse(responseCode = "401", description = "Cannot delete another user's post"),
+    @APIResponse(responseCode = "404", description = "User / post does not exist")
+  })
+  public Response deletePostEndpoint(@HeaderParam("X-User") UUID userId) // TODO add postId as body
+      {
+    return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+  }
 
-    @GET
-    @Path("/posts/{authorId}")
-    @APIResponses({
-            @APIResponse(responseCode = "200", description = "OK"),
-            @APIResponse(responseCode = "400", description = "Bad author format"),
-            @APIResponse(responseCode = "404", description = "Author does not exist")
-    })
-    public Response queryUserPostsEndpoint(@PathParam("authorId") UUID authorId)
-    {
-        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
-    }
+  @GET
+  @Path("/posts/{authorId}")
+  @APIResponses({
+    @APIResponse(responseCode = "200", description = "OK"),
+    @APIResponse(responseCode = "400", description = "Bad author format"),
+    @APIResponse(responseCode = "404", description = "Author does not exist")
+  })
+  public Response queryUserPostsEndpoint(@PathParam("authorId") UUID authorId) {
+    return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+  }
 
-    @GET
-    @Path("/post/{postId}")
-    @APIResponses({
-            @APIResponse(responseCode = "200", description = "OK"),
-            @APIResponse(responseCode = "400", description = "Bad post format"),
-            @APIResponse(responseCode = "404", description = "Post does not exist")
-    })
-    public Response queryPostEndpoint(@PathParam("postId") UUID postId)
-    {
-        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
-    }
+  @GET
+  @Path("/post/{postId}")
+  @APIResponses({
+    @APIResponse(responseCode = "200", description = "OK"),
+    @APIResponse(responseCode = "400", description = "Bad post format"),
+    @APIResponse(responseCode = "404", description = "Post does not exist")
+  })
+  public Response queryPostEndpoint(@PathParam("postId") UUID postId) {
+    return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+  }
 
-    @GET
-    @Path("/replies/{postId}")
-    @APIResponses({
-            @APIResponse(responseCode = "200", description = "OK"),
-            @APIResponse(responseCode = "400", description = "Bad post format"),
-            @APIResponse(responseCode = "404", description = "Post does not exist")
-    })
-    public Response queryPostRepliesEndpoint(@PathParam("postId") UUID postId)
-    {
-        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
-    }
+  @GET
+  @Path("/replies/{postId}")
+  @APIResponses({
+    @APIResponse(responseCode = "200", description = "OK"),
+    @APIResponse(responseCode = "400", description = "Bad post format"),
+    @APIResponse(responseCode = "404", description = "Post does not exist")
+  })
+  public Response queryPostRepliesEndpoint(@PathParam("postId") UUID postId) {
+    return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+  }
 }
--- tinyX/repo-post/src/test/java/com/tinyx/PostTests.java
+++ tinyX/repo-post/src/test/java/com/tinyx/PostTests.java
@@ -1,48 +1,49 @@
 package com.tinyx;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
-import jakarta.ws.rs.core.MediaType;
-
-import java.util.UUID;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
 /**
- * Implements tests regarding the repo-post service.
- * In the future when more tests are added, this file may need to be split into
- * individual endpoint testing files.
+ * Implements tests regarding the repo-post service. In the future when more tests are added, this
+ * file may need to be split into individual endpoint testing files.
  *
- * Please add tests here as you implement/fix/work on stuff.
+ * <p>Please add tests here as you implement/fix/work on stuff.
  */
 @QuarkusTest
 public class PostTests {
-    @Test
-    public void testApp() {
-        assertTrue(true, "This is a basic test with JUnit 5");
-    }
+  @Test
+  public void testApp() {
+    assertTrue(true, "This is a basic test with JUnit 5");
+  }
 
-    @Test
-    public void simpleNewPost()
-    {
-        UUID userId = UUID.randomUUID();
-        given().contentType(MediaType.APPLICATION_JSON)
-                .header("X-User", userId).body("{ \"text\" : \"FIXME\" }")
-                .when().post("/posts/new")
-                .then()
-                .statusCode(200).body("authorId", equalTo(userId.toString())); // Is this how it's going to work ?
-    }
+  @Test
+  public void simpleNewPost() {
+    UUID userId = UUID.randomUUID();
+    given()
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("X-User", userId)
+        .body("{ \"text\" : \"FIXME\" }")
+        .when()
+        .post("/posts/new")
+        .then()
+        .statusCode(200)
+        .body("authorId", equalTo(userId.toString())); // Is this how it's going to work ?
+  }
 
-    @Test
-    public void simpleQueryPost()
-    {
-        UUID postId = UUID.randomUUID();
-        given().when().get("/posts/post/" + postId.toString())
-                .then().statusCode(200).body("postId", equalTo(postId));
-    }
+  @Test
+  public void simpleQueryPost() {
+    UUID postId = UUID.randomUUID();
+    given()
+        .when()
+        .get("/posts/post/" + postId.toString())
+        .then()
+        .statusCode(200)
+        .body("postId", equalTo(postId));
+  }
 }

7909757313e4f0e9f13a82b4ac321264d70e3a24
Bastien Gaulier <bastien.gaulier@epita.fr>
2025-03-26 16:51:12 +0100

    feat(repo-post): added endpoint skeleton for querying a list of posts

--- tinyX/repo-post/src/main/java/com/tinyx/controller/PostController.java
+++ tinyX/repo-post/src/main/java/com/tinyx/controller/PostController.java
@@ -2,6 +2,7 @@ package com.tinyx.controller;
 
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+import java.util.List;
 import java.util.UUID;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
@@ -38,6 +39,24 @@ public class PostController {
   })
   public Response deletePostEndpoint(@HeaderParam("X-User") UUID userId) // TODO add postId as body
       {
+    return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+  }
+
+  /**
+   * Uses a list of post IDs to fetch and return the corresponding post entities.
+   *
+   * @param userId The user querying the endpoint
+   * @param postIds The list of post IDs to fetch
+   * @return The corresponding posts extracted from mongoDB
+   */
+  @GET
+  @Path("/posts")
+  @APIResponses({
+    @APIResponse(responseCode = "200", description = "OK"),
+    @APIResponse(responseCode = "400", description = "Bad post ID inside of the list"),
+    @APIResponse(responseCode = "404", description = "A post ID does not link to an existing post")
+  })
+  public Response queryPostsList(@HeaderParam("X-User") UUID userId, List<UUID> postIds) {
     return Response.status(Response.Status.NOT_IMPLEMENTED).build();
   }
 

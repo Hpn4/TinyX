263675e45e0cf7c3c2c378de21af15e3dfcd7663
Guillaume Hanry <guillaume.hanry@epita.fr>
2025-03-28 17:53:43 +0100

    (add) Repo creation: created repo-post and social

---
+++ tinyX/repo-social/src/test/java/com/tinyx/RepoSocialTests.java
@@ -0,0 +1,20 @@
+package com.tinyx;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Implements tests regarding the repo-post service. In the future when more tests are added, this
+ * file may need to be split into individual endpoint testing files.
+ *
+ * <p>Please add tests here as you implement/fix/work on stuff.
+ */
+@QuarkusTest
+public class RepoSocialTests {
+  @Test
+  public void testApp() {
+    assertTrue(true, "This is a basic test with JUnit 5");
+  }
+}

7c6c29ababdd166b2235eb1561d2f052259a622b
Kendrick Ory <kendrick.ory@epita.fr>
2025-04-12 23:02:53 +0200

    javadoc of repo-home-timeline/repo-social/repo-media/repo-search/srvc-media/srvc-home-timeline/srvc-search

--- tinyX/repo-social/src/main/java/com/tinyx/converter/PostQueryToPostEntityConverter.java
+++ tinyX/repo-social/src/main/java/com/tinyx/converter/PostQueryToPostEntityConverter.java
@@ -9,10 +9,22 @@ import java.util.stream.Collectors;
 @ApplicationScoped
 public class PostQueryToPostEntityConverter {
 
+  /**
+   * Convert a PostQuery to a PostEntity.
+   *
+   * @param postQuery the PostQuery to convert.
+   * @return the PostQuery converted into a PostEntity
+   */
   public PostEntity convert(PostQuery postQuery) {
     return new PostEntity(postQuery.post.id, postQuery.post.userId);
   }
 
+  /**
+   * Convert multiple PostQuery into a list of PostEntity
+   *
+   * @param postQueryList list of PostQuery to convert
+   * @return the list of PostQuery converted into a list of PostEntity
+   */
   public List<PostEntity> convert(List<PostQuery> postQueryList) {
     return postQueryList.stream().map(this::convert).collect(Collectors.toList());
   }
--- tinyX/repo-social/src/main/java/com/tinyx/repository/entity/PostEntity.java
+++ tinyX/repo-social/src/main/java/com/tinyx/repository/entity/PostEntity.java
@@ -2,6 +2,7 @@ package com.tinyx.repository.entity;
 
 import java.util.UUID;
 
+/** Class that represent a post node in the neo4j database. */
 public class PostEntity {
   public UUID id;
 
